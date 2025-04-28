c7b0f243fcae40a6aeb39f5e6a07b840c7a4810f
Tomas Blazek <tomasblazekmr@gmail.com>
2025-04-28 09:25:16 +0200

    fibo works but why

--- fibo.py
+++ fibo.py
@@ -1,5 +1,17 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return n
+    if n == 1:
+        return n
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
+
+
 
 def main():
+    f = recursive_nth_fibo(7)
+    print(f)
     pass
 
 
